19a47b8f862f00d6535a03a4abd72bf0d3b66aa3
himabindus <himabindu@deepcompute.com>
2017-09-28 17:02:50 +0200

    references #3 modified the forwarder.py code

--- logagg/forward/forwarder.py
+++ logagg/forward/forwarder.py
@@ -21,14 +21,29 @@ class LogForwarder(BaseScript):
     MAX_SECONDS_TO_PUSH = 2
     MAX_MESSAGES_TO_PUSH = 100
 
-    def run(self):
+    def __init__(self, log, args, nsqtopic, nsqchannel, nsqd_tcp_address, mongodb_server_url,\
+            mongodb_port, mongodb_user_name, mongodb_password, mongodb_database, mongodb_collection):
+
+        self.log = log
+        self.args = args
+        self.nsqtopic = nsqtopic
+        self.nsqchannel = nsqchannel
+        self.nsqd_tcp_address = nsqd_tcp_address
+        self.mongodb_server_url = mongodb_server_url
+        self.mongodb_port = mongodb_port
+        self.mongodb_user_name = mongodb_user_name
+        self.mongodb_password = mongodb_password
+        self.mongodb_database = mongodb_database
+        self.mongodb_collection = mongodb_collection
+
+    def start(self):
 
         # Establish connection to MongoDB to store the nsq messages
-        url = 'mongodb://%s:%s@%s:%s' % (self.args.mongodb_user_name, self.args.mongodb_password,
-                self.args.mongodb_server_url, self.args.mongodb_port)
+        url = 'mongodb://%s:%s@%s:%s' % (self.mongodb_user_name, self.mongodb_password,
+                self.mongodb_server_url, self.mongodb_port)
         client = MongoClient(url, serverSelectionTimeoutMS=self.SERVER_SELECTION_TIMEOUT)
-        self.mongo_database = client[self.args.mongodb_database]
-        self.mongo_coll = self.mongo_database[self.args.mongodb_collection]
+        self.mongo_database = client[self.mongodb_database]
+        self.mongo_coll = self.mongo_database[self.mongodb_collection]
 
         # Initialize a queue to carry messages between the
         # producer (nsq_reader) and the consumer (read_from_q)
@@ -36,9 +51,9 @@ class LogForwarder(BaseScript):
 
         # Establish connection to nsq from where we get the logs
         self.nsq_reader = nsq.Reader(
-            topic=self.args.nsqtopic,
-            channel=self.args.nsqchannel,
-            nsqd_tcp_addresses=self.args.nsqd_tcp_address
+            topic=self.nsqtopic,
+            channel=self.nsqchannel,
+            nsqd_tcp_addresses=self.nsqd_tcp_address
         )
         self.nsq_reader.set_message_handler(self.handle_msg)
         self.nsq_reader.set_max_in_flight(self.MAX_IN_FLIGHT)
@@ -110,22 +125,3 @@ class LogForwarder(BaseScript):
             self.log.info("inserted the msgs into mongodb %d" % (len(msgs)))
         except pymongo.errors.BulkWriteError as bwe:
             self.log.exception('Write to mongo failed. Details: %s' % bwe.details)
-
-    def define_args(self, parser):
-
-        parser.add_argument('--nsqtopic', help='NSQ topic name to read messages from. Ex: logs_and_metrics')
-        parser.add_argument('--nsqchannel', help='channel of nsqd')
-        parser.add_argument('--nsqd-tcp-address', help='nsqd tcp address where we get the messages')
-
-        parser.add_argument('--mongodb-server-url', help='DNS of the server where mongo is running')
-        parser.add_argument('--mongodb-port', help='port where mongo is running')
-        parser.add_argument('--mongodb-user-name', help='username of mongodb')
-        parser.add_argument('--mongodb-password', help='password to authenticate mongodb')
-        parser.add_argument('--mongodb-database', help='database to store logs')
-        parser.add_argument('--mongodb-collection', help='collection to store logs')
-
-def main():
-    LogForwarder().start()
-
-if __name__ == '__main__':
-    main()
